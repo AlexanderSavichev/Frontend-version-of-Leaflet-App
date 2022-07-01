eb6f88da70ba4be850467b51ed92ea4af03b73fd
Egor Smirnov <smirnov.egor.v@gmail.com>
2022-07-01 13:45:52 +0300

    Маркер сохраняется в отдельную переменную Если идет клик на карте, а маркер уже есть - он передвигается Координаты в методе отправки данных берутся из position

--- src/map.js
+++ src/map.js
@@ -1,65 +1,71 @@
 
 import { useState } from 'react'
 import {
-  MapContainer,
-  Marker,
-  Popup,
-  TileLayer,
-  useMapEvents,
+    MapContainer,
+    Marker,
+    Popup,
+    TileLayer,
+    useMapEvents,
 } from 'react-leaflet'
 import L from 'leaflet';
 import './map.css';
 
 L.Icon.Default.imagePath = "https://unpkg.com/leaflet@1.5.0/dist/images/";
 function LocationMarker() {
-  const [position, setPosition] = useState(null)
+    const [position, setPosition] = useState(null)
+    const [marker, setMarker] = useState(null)
 
-  const handleClick=()=>{
-    fetch("http://localhost:8080/mapModel/add",{
-        method:"POST",
-        headers:{"Content-Type":"application/json"},
-        body:JSON.stringify(coord)
-    }).then(()=>{
-        console.log("New task added", coord)
-        setPosition([...position,coord]);
+    const handleClick=()=>{
+        const { lat, lng } = position;
+        let coord = {lat, lng}
+        console.log("handleClick() " + JSON.stringify(coord))
+        fetch("http://localhost:8080/mapModel/add",{
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(coord)
+        }).then(()=>{
+            console.log("New task added", coord)
+            setPosition([...position,coord]);
+        })
+    }
+
+
+    const map = useMapEvents({
+        click(e) {
+            console.log("click()" + e)
+            console.log("coord = " + JSON.stringify(position))
+            if (position != null) {
+                // remove existing marker
+                marker.setLatLng(e.latlng)
+            } else {
+                let m = L.marker(e.latlng)
+                    .addTo(map);
+                setMarker(m)
+            }
+            setPosition(e.latlng)
+
+        }
     })
-}
 
-
-  const map = useMapEvents({
-    click(e) {
-      
-
-
-      const { lat, lng } = e.latlng;
-      const coord = {lat,lng};
-      setPosition(e.latlng)
-     // console.log(lat, lng);
-  L.marker(e.latlng)
-  //.bindPopup(textbox + '</br>' + template)
-  .addTo(map);
-    },
-  })
-
-  return position === null ? null : (
-    <Marker position={position}>
-      console.log(position);
-      <Popup><input id="time" type="text" placeholder="Enter something" />
-      <button  class="edit" id="buttonEdit" type="button" onClick={handleClick} >Submit</button>
-      </Popup>
-    </Marker>
-  )
+    return position === null ? null : (
+        <Marker position={position}>
+            console.log(position);
+            <Popup><input id="time" type="text" placeholder="Enter something" />
+                <button  class="edit" id="buttonEdit" type="button" onClick={handleClick} >Submit</button>
+            </Popup>
+        </Marker>
+    )
 }
 
 function EventsExample() {
-  return (
-    <MapContainer center={{ lat: 59.9311, lng:30.3609 }} zoom={12}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <LocationMarker />
-    </MapContainer>
-  )
+    return (
+        <MapContainer center={{ lat: 59.9311, lng:30.3609 }} zoom={12}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <LocationMarker />
+        </MapContainer>
+    )
 }
 export default EventsExample;
